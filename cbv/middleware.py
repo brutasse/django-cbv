c3b48b1a4637fa3d2d3639bda300af4d896b21dd
Mikko Hellsing <mikko@aino.se>
2010-12-10 12:38:57 +0100

    making middleware handle errors

--- cbv/middleware.py
+++ cbv/middleware.py
@@ -1,8 +1,30 @@
+
 class DeferredRenderingMiddleware(object):
     def process_response(self, request, response):
-        # If the response supports deferred rendering, apply template
-        # response middleware and the render the response
-        if hasattr(response, 'render') and callable(response.render):
-            return response.render()
-        return response
+        try:
+            # If the response supports deferred rendering, apply template
+            # response middleware and the render the response
+            if hasattr(response, 'render') and callable(response.render):
+                return response.render()
+            return response
+        except Exception:
+            import sys
+            from django.conf import settings
+            from django.core.handlers.base import BaseHandler
+            from django.core import signals
+            from django.core import urlresolvers
+
+            urlconf = settings.ROOT_URLCONF
+            urlresolvers.set_urlconf(urlconf)
+            resolver = urlresolvers.RegexURLResolver(r'^/', urlconf)
+            receivers = signals.got_request_exception.send(
+                sender=self.__class__,
+                request=request,
+                )
+            handler = BaseHandler()
+            return handler.handle_uncaught_exception(
+                request,
+                resolver,
+                sys.exc_info(),
+                )
 
